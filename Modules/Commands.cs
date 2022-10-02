51671893818567ee08209ec044e265de4020605d
jedrek <jedrek@LAPTOP-HGKA0LEI>
2022-10-02 22:07:39 +0200

    Dodaj pliki projektów.

---
+++ Modules/Commands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace JedrusBotDiscord.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        private string _path = "../../../Quotes.txt";
+
+        [Command("quote")]
+        public async Task GetRandomQuote()
+        {
+            var lines = File.ReadAllLines(_path);
+            var r = new Random();
+            var randomLineNumber = r.Next(0, lines.Length - 1);
+            var line = lines[randomLineNumber];
+            await ReplyAsync(line);
+        }
+
+        [Command("dupa")]
+        public async Task SayDupa()
+        {
+            
+        }
+    }
+}
